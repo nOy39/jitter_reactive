643ddeff7efcbb5e2f6a3df9953b30ea0a3a7e62
Andrey <popov.a83@bk.ru>
2018-07-09 13:01:22 +0200

    bug

---
+++ backend/src/main/java/org/a2lpo/jitter_reactive/controller/BackController.java
@@ -0,0 +1,36 @@
+package org.a2lpo.jitter_reactive.controller;
+
+import org.a2lpo.jitter_reactive.entities.User;
+import org.a2lpo.jitter_reactive.repos.UserRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    private static final String HELLO_TEXT = "Hello from Spring Boot Backend!";
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping(path = "/hello")
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody String sayHello() {
+        LOG.info("GET called on /hello resource");
+        return HELLO_TEXT;
+    }
+    @RequestMapping(path = "/user", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody User getAllUser() {
+        List<User> all = userRepo.findAll();
+        User admin = userRepo.findByUsername("admin");
+        return admin;
+    }
+}
